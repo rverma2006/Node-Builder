e4978b9260e8b2e964511cad28e911051cd062c6
Ritvik <v>
2026-08-25 22:10:27 +0400

    Public

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host:    process.env.DB_HOST || "173.249.53.129",
+    host:    process.env.DB_HOST,
     port:    process.env.DB_PORT || 3306,
     dialect: "mysql",
     logging: false,
